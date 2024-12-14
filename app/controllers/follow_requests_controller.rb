75c35a71668df75a20bae7f9c3a15f52223f5c04
sdvillamil <156153167+sdvillamil@users.noreply.github.com>
2024-12-14 03:56:55 +0000

    1/2 controller, break

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,2 +1,62 @@
 class FollowRequestsController < ApplicationController
+  def index
+    matching_follow_requests = FollowRequest.all
+
+    @list_of_follow_requests = matching_follow_requests.order({ :created_at => :desc })
+
+    render({ :template => "follow_requests/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_follow_requests = Photo.where({ :id => the_id })
+
+    @the_follow_request = matching_follow_requests.at(0)
+
+    render({ :template => "follow_requests/show.html.erb" })
+  end
+
+  def create
+    the_follow_request = FollowRequest.new
+
+    the_follow_request.recipient_id = params.fetch("query_recipient_id")
+
+    the_follow_request.sender_id = params.fetch("query_sender_id")
+    
+    the_follow_request.status = params.fetch("query_status")
+
+
+    if if the.valid?
+      the_photo.save
+      redirect_to("/folow_request", { :notice => "Photo created successfully." })
+    else
+      redirect_to("/photos", { :alert => the_photo.errors.full_messages.to_sentence })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_photo = Photo.where({ :id => the_id }).at(0)
+
+    the_photo.caption = params.fetch("query_caption")
+    the_photo.image = params.fetch("query_image")
+
+    if the_photo.valid?
+      the_photo.save
+      redirect_to("/photos/#{the_photo.id}", { :notice => "Photo updated successfully."} )
+    else
+      redirect_to("/photos/#{the_photo.id}", { :alert => the_photo.errors.full_messages.to_sentence })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_photo = Photo.where({ :id => the_id }).at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos", { :notice => "Photo deleted successfully."} )
+  end
+end
 end
